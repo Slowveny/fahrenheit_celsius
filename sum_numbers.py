4eba0dc8defe10237b8b7559c40c7508874ecce4
Slowveny <victorchandiabacks@gmail.com>
2025-09-26 23:39:25 -0300

    fahrenheit_convert_celsius

--- sum_numbers.py
+++
@@ -1,4 +0,0 @@
-number_1 = float(input('Enter a first number: '))
-number_2 = float(input('Enter a second number: '))
-result = number_1 + number_2
-print(f'The sum of number {number_1} and {number_2} is {result}')
